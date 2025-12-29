ae846fd596a14f38e6d2c4a059ee55d0de815902
ranjithranjith2004 <mranjithips957@gmail.com>
2025-12-29 07:30:17 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,40 @@
+// A Function Declaration in JavaScript is a way to define a function using the function keyword with a function name
+
+function a (){
+  console.log("SLA");
+}
+
+a();
+
+function Name() {                ---(string)---
+  console.log("heloo sla");
+}
+
+Name();
+
+function sum() {
+  console.log(14 + 2.2); ---(Number)---
+}
+
+sum(); 
+
+function sla(name) {
+  console.log("Hello " + name);   ---(parameters)---
+}
+
+sla("Ram"); 
+
+function aa(num) {
+  return num * num;
+}                          ----(return)---
+
+console.log(aa(2)); 
+
+
+
+
+
+
+
+
+
---
+++ function.js
@@ -0,0 +1,42 @@
+// Function Declaration
+
+function a() {
+  console.log("Hello");
+}
+
+a(); 
+
+
+// Function Expression
+
+const b = function () {
+  console.log("hii");
+};
+
+b(); 
+
+// Arrow Function
+
+const c = () => {
+  console.log("hi sla");
+};
+
+c(); 
+
+
+// Parameters
+
+function add(a, b) {   
+  return a + b;
+}
+
+add(33, 3);
+
+// Arguments
+
+function cc(name) {  
+  console.log("Hello " + name);
+}
+
+cc("Ravi"); 
+
